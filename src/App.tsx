90afe9de4795755d071b5ca9157ff0f00251f547
Danielle Hull <danielleparker2008@gmail.com>
2023-08-03 13:50:27 -0500

    Set up initial api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,50 @@
 import { Container } from "@mui/material";
 import Layout from "./components/Layout";
 import Header from "./components/Header";
+import React, { useState, useEffect } from 'react';
+import { WordleRequestItem, WordleResponse, fetchWordleResult } from './api/api';
 
 function App() {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [words, setWords] = useState<string[]>([]);
+
+    function handleSuccess(response: WordleResponse) {
+      console.log('success');
+      console.log(response);
+      setWords((prev) => [response.guess, ...prev]);
+      setLoading(false);
+    }
+
+    function handleError(error: Error) {
+      console.log('error');
+      console.log(error);
+      setError(true);
+    }
+
+    function handleWordleRequest(word: string, clue: string) {
+      const requestItem: WordleRequestItem = { word, clue };
+
+      fetchWordleResult([requestItem])
+        .then(handleSuccess, handleError)
+        .catch(handleError);
+    }
+
+    function generateStartingWord(): string {
+      let startingWord = '';
+
+      for (let i = 0; i < 5; i++) {
+        let offset = Math.floor(Math.random() * 26);
+        startingWord += String.fromCharCode(97 + offset);
+      }
+
+      return startingWord;
+    }
+
+    useEffect(() => {
+      handleWordleRequest(generateStartingWord(), 'xxxxx');
+    }, []);
+
     return (
         <Layout>
             <Container maxWidth="sm">
