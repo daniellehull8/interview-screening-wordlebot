ca1913f4dd1c99cca8cfcf07eaf61c672a8ffb90
Danielle Hull <danielleparker2008@gmail.com>
2023-08-04 19:45:47 -0500

    add color select group

---
+++ src/components/ColorSelectGroup/ColorSelectGroup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ColorSelectGroup() {
+  return (
+    <>
+      <p>White</p>
+      <p>Yellow</p>
+      <p>Green</p>
+    </>
+  );
+}
+
+export default ColorSelectGroup;
--- src/components/Word/Word.js
+++ src/components/Word/Word.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Letter from '../Letter/Letter';
+import ColorSelectGroup from '../ColorSelectGroup/ColorSelectGroup';
 
 function Word(props) {
   const letters = [...props.word];
   return (
     <>
       {letters.map((letter, index) =>
-        <Letter key={props.word + letter + index} letter={letter} />
+        <>
+          <Letter key={props.word + letter + index} letter={letter} />
+          <ColorSelectGroup />
+        </>
       )}
     </>
   );
