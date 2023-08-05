68adc1bf71a3ba2b4e7abaf2580ac5ceb730b4c6
Danielle Hull <danielleparker2008@gmail.com>
2023-08-04 19:34:44 -0500

    added the word component

---
+++ src/components/Word/Word.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Word(props) {
+  return (
+    <>
+      Here is a word in word component: {props.word}.
+    </>
+  );
+}
+
+export default Word;
--- src/components/WordleBoard/WordleBoard.js
+++ src/components/WordleBoard/WordleBoard.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Word from '../Word/Word';
 
 function WordleBoard(props) {
   return (
     <>
       <h1>This is the wordle board</h1>
-      <p>Here are some words: {props.words}</p>
+      {props.words.map((word, index) =>
+        <Word key={word + index} word={word} />
+      )}
     </>
   );
 }
